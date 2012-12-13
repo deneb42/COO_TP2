6a79620acf962bbce787749838585f53ba6aab31
deneb42 <benjblois@hotmail.com>
2012-12-13 22:17:51 +0100

    started spec of produits

--- src/produit/High_Tech.java
+++ src/produit/High_Tech.java
@@ -1,5 +1,15 @@
 package produit;
 
 public class High_Tech extends Produit {
-
+	String specTechniques;
+	
+	
+	public High_Tech(int id, String n, String sp) {
+		super(id, n);
+		specTechniques = sp;
+	}
+	
+	public String toString() {
+		return super.toString() + specTechniques;
+	}
 }
